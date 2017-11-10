a5ac74c449aa608ac0d24793a8662a2d9f291029
WoogWalker <Richard.Shneyder.77@gmail.com>
2017-11-10 04:55:05 +0200

    FullRect

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        drawLine(10, '_');
+
+//        drawLine(9, '_');
+//        System.out.println("");
+//        drawLine(9, '_');
+//        System.out.println("");
+//        drawLine(9, '_');
+
+        drawRect(5, '-');
     }
 
     public static void drawLine (int a, char b){
-        for (int p = 0; p <= a; p++){
+        for (int p = 0; p < a; p++){
             System.out.print(b);
         }
     }
 
     public static void drawRect(int c, char d){
-
+        for(int w = 0; w < c; w++){
+            drawLine(c, '=');
+            System.out.println();
+        }
     }
 }
